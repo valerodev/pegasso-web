c7025a69f88dd6fcd82b91a2607be638c8f27733
Daniel Valero <danielvalerogenil@gmail.com>
2026-02-23 09:59:28 +0100

    Update close-menu.js

--- js/close-menu.js
+++ js/close-menu.js
@@ -1,9 +1,11 @@
-const enlacesNav = document.querySelectorAll('.enlace-nav');
+const enlacesNav = document.querySelectorAll('.item-menu');
+const checkboxMenu = document.querySelector('#control-menu');
 
 enlacesNav.forEach(enlace => {
     enlace.addEventListener('click', () => {
-        menuCompleto.classList.remove('menu-abierto');
-        botonAbrir.classList.remove('menu-activo');
-        document.body.style.overflow = '';
+        if (checkboxMenu) {
+            checkboxMenu.checked = false;
+        }
+        document.body.style.overflow = 'auto';
     });
 });
